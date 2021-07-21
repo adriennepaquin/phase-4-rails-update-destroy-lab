5f315a0f75baebbce3123821933842e2fb8f667e
Adrienne <adriennepaquin@gmail.com>
2021-07-20 20:53:35 -0400

    commit tree f7da0dd894f50920641f4ced25ec3dc2e36feec0

---
+++ db/migrate/20210721004949_add_is_in_stockto_plants.rb
@@ -0,0 +1,4 @@
+class AddIsInStocktoPlants < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_03_214928) do
+ActiveRecord::Schema.define(version: 2021_07_21_004949) do
 
   create_table "plants", force: :cascade do |t|
     t.string "name"
